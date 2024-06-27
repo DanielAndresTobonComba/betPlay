4e69889156c153d8dc8279582d70015ece161d9b
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-06-27 11:13:08 -0500

    empezando

--- betplay/src/main/java/com/betplay/Main.java
+++ betplay/src/main/java/com/betplay/Main.java
@@ -2,22 +2,6 @@ package com.betplay;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Variables globales
-        int desicionMenu = 0;
-
-
-            
-
-
-        do {
-            
-            System.out.println("**********Menu de inicio**********");
-            System.out.println("1. Registros");
-            System.out.println("2.");
-                
-            
-        } while (true);
-
+        System.out.println("Hello world!");
     }
 }
