2f3139c263909108ffca298cc287e05b9adc8578
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-06-27 14:55:52 -0500

    listoso view admin

---
+++ betplay/src/main/java/com/betplay/View/Administrador.java
@@ -0,0 +1,24 @@
+package com.betplay.View;
+
+public class Administrador {
+
+    public static void star(){
+
+        do {
+            
+            System.out.println("1.Registro de usuarios");
+            System.out.println("2. Gestion de Equipos");
+            System.out.println("3. Gestion de jugadores");
+            System.out.println("4. Gestion de partidos");
+            System.out.println("5. Gestion de arbitros");
+            System.out.println("6. Gestion de estadios");
+            System.out.println("7. Gestion de patrocinios");
+            System.out.println("8. Getion de sanciones");
+            System.out.println("9. Gestion de comunicados y noticias");
+            
+
+        } while (true);
+    }
+
+
+}
