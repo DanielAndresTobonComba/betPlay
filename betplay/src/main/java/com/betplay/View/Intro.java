dad1b984e3c9e0d1b7e6b3c9b95686ad794b31e7
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-06-30 22:03:02 -0500

    ultima modificacion

--- betplay/src/main/java/com/betplay/View/Intro.java
+++ betplay/src/main/java/com/betplay/View/Intro.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 
 import com.betplay.View.ViewRoles.seleccionRol;
 
-<<<<<<< HEAD
 import com.betplay.Entity.CheckInt;
 
-=======
->>>>>>> 9e4a932a81a703bce58ab9c241dbed9701dc80bd
 public class Intro {
 
     public static void startIntro (){
@@ -29,7 +26,6 @@ public class Intro {
         System.out.println("3. Salir");
         System.out.println("-------------------");
 
-<<<<<<< HEAD
         System.out.println("\n\n. . . . . . . . . .");
         System.out.println(" Digite la opción");
         System.out.println(". . . . . . . . . . ");
@@ -38,16 +34,6 @@ public class Intro {
         // Validación de la opción ingresada por el usuario
         decision = CheckInt.check(introPermise);
         
-=======
-            System.out.println("\n\tMENU\n");
-            System.out.println("1. Registrarse");
-            System.out.println("2. Iniciar sesion");
-            System.out.println("3. Salir");
-
-            System.out.print("Opcion: ");
-            decision = scanner.nextInt();
-            scanner.nextLine();
->>>>>>> 9e4a932a81a703bce58ab9c241dbed9701dc80bd
      
             
         //} while (decision != 3);
@@ -59,34 +45,9 @@ public class Intro {
 
             case 2:
 
-<<<<<<< HEAD
             System.out.println("Iniciar sesión");
                 break;
 
-=======
-                seleccionRol.startSeleccionRol();
-                
-                break;
-
-            case 3:
-
-                System.out.println("Programa finalizado");
-                scanner.close();
-                
-                break;
-
-            case 4:
-                
-                break;
-
-            case 5:
-                
-                break;
-
-            case 6:
-                
-                break;
->>>>>>> 9e4a932a81a703bce58ab9c241dbed9701dc80bd
         
             default:
             System.out.println("CHAO");
